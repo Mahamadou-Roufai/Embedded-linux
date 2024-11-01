8b425e6130d9b4933440847f9a39b3a218a74fb3
Mahamadou_Roufai <garbachipkao_mah@upf.ac>
2024-11-01 01:56:37 +0100

    All commited

---
+++ build/assignment-autotest/CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o.d
@@ -0,0 +1,3 @@
+assignment-autotest/CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o: \
+ /home/roufai/Roufai/Autoformation/Linux/Devoir/assignment-autotest/test/unity_runner.c \
+ /usr/include/stdc-predef.h
